b69376511dd5a0fd3863396c6ff29a12d49c210f
Pooja <poojavijay2475@gmail.com>
2024-10-10 00:03:54 +0530

    My files

---
+++ src/Oct/ex_09102024/Task001.java
@@ -0,0 +1,27 @@
+package Oct.ex_09102024;
+
+public class Task001 {
+    public static void main(String[] args) {
+        int s = 7;
+        System.out.printf("%d * %d = %d", s , 1, s);
+        System.out.println();
+        System.out.printf("%d * %d = %d", s , 2, s*2);
+        System.out.println();
+        System.out.printf("%d * %d = %d", s , 3, s*3);
+        System.out.println();
+        System.out.printf("%d * %d = %d", s , 4, s*4);
+        System.out.println();
+        System.out.printf("%d * %d = %d", s , 5, s*5);
+        System.out.println();
+        System.out.printf("%d * %d = %d", s , 6, s*6);
+        System.out.println();
+        System.out.printf("%d * %d = %d", s , 7, s*7);
+        System.out.println();
+        System.out.printf("%d * %d = %d", s , 8, s*8);
+        System.out.println();
+        System.out.printf("%d * %d = %d", s , 9, s*9);
+        System.out.println();
+        System.out.printf("%d * %d = %d", s , 10, s*10);
+        System.out.println();
+    }
+}
