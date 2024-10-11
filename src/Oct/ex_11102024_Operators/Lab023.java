9901e36ef0d6e9263a69391c8e95fc6944c268cf
Pooja <poojavijay2475@gmail.com>
2024-10-11 16:54:40 +0530

    My files

---
+++ src/Oct/ex_11102024_Operators/Lab023.java
@@ -0,0 +1,16 @@
+package Oct.ex_11102024_Operators;
+
+public class Lab023 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b); // Mathimatics Sum
+
+        String name1 = "Pramod";
+        String name2 = "Dutta";
+        System.out.println(name1 + name2); // concatination
+
+        // + -> behave differently with the data type.
+        // + -> operator overloading
+    }
+}
---
+++ src/Oct/ex_11102024_Operators/Lab026.java
@@ -0,0 +1,10 @@
+package Oct.ex_11102024_Operators;
+
+public class Lab026 {
+    public static void main(String[] args) {
+        int balaji_salary = 20000;
+        boolean b = !(balaji_salary > 10000 || balaji_salary < 5000);
+        System.out.println(b);
+    }
+}
+
