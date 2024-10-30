39bf76d5847e244a0f3725b0f02ea0ce2e0d4969
Pooja <poojavijay2475@gmail.com>
2024-10-30 23:48:26 +0530

    My files

---
+++ src/Oct/ex_29102024_Arrays/Lab092.java
@@ -0,0 +1,17 @@
+package Oct.ex_29102024_Arrays;
+
+public class Lab092 {
+    public static void main(String[] args) {
+
+        int[] marks = {51, 100, 91, 87, 90, 91, 92};
+        // 0 to 6
+        // Arrays Creation with the pre-defined elements.
+        System.out.println(marks[0]);
+        System.out.println(marks[4]);
+        //System.out.println(marks[10]); // ArrayIndexOutOfBoundsException
+
+        System.out.println(marks.length); //length 1
+
+    }
+}
+
---
+++ src/Oct/ex_29102024_Arrays/Lab097_Interview.java
@@ -0,0 +1,16 @@
+package Oct.ex_29102024_Arrays;
+
+import java.util.Arrays;
+
+public class Lab097_Interview {
+    public static void main(String[] args) {
+
+        int[] array = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
+        Arrays.sort(array);
+        System.out.println(array[array.length-1]);
+
+
+    }
+
+}
+
