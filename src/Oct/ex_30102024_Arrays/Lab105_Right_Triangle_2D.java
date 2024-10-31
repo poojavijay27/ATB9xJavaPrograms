56502492c84f21846f45bce9884080061cdd9bf8
Pooja <poojavijay2475@gmail.com>
2024-10-31 16:25:23 +0530

    My files

---
+++ src/Oct/ex_30102024_Arrays/Lab105_Right_Triangle_2D.java
@@ -0,0 +1,29 @@
+package Oct.ex_30102024_Arrays;
+
+import java.util.Scanner;
+
+public class Lab105_Right_Triangle_2D {
+    public static void main(String[] args) {
+
+        //*
+        //**
+        //***
+        //****
+        //*****
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number n=5");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+
+    }
+}
+
